5e050c69151b2343c4b217dca4cbda6d3ca9446b
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-05-11 18:00:30 -0400

    Defined Structure for Data Collection

---
+++ src/data_collection.py
@@ -0,0 +1,28 @@
+
+# Gather Stuff Here
+
+
+def Main():
+    # Get Information about Cat
+    # Get Information about Trip
+    # Get Information about Fuel Prices
+
+    # Format Information
+
+    # Create file called 'trip_info'
+
+    # Write Fuel Economy, Fuel Capacity, Trip Budget, and Number 
+    # of Stops on 1 line each seperated by single spaces
+
+    # Write distance from current stop to next stop for all stops
+    # Write time from current stop to next stop for all stops
+
+    # Write time taken from the highway to the fuel stop back 
+    # to the highway for all stops
+
+    # Write Cost of Fuel
+
+    # End Data Collection Phase
+    pass
+
+Main()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+
+def Main():
+    # Ask user what their starting point is 
+    # Ask user what their ending point is 
+    # Ask user what their budget is
+    # Ask user what their car model is
+
+    # Validate Inputs along the way
+
+    # Begin Data Collection Phase
+    # (Data Collection Phase ends)
+
+    # Begin Calculation Phase
+    # (Calculation Phase ends)
+
+    
+    pass
+
+Main()
